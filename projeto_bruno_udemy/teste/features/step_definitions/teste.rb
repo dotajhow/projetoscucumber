d3b70750c74ad36842d54292884b38ba80069fd7
dotajhow <deia-jesus@hotmail.com>
2021-07-24 15:51:11 -0300

    remover arquivo

--- projeto_bruno_udemy/teste/features/step_definitions/teste.rb
+++
@@ -1,8 +0,0 @@
-Quando(/^eu somo (\d+) \+ (\d+)$/) do |arg1, arg2|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
-  Então(/^o resultado tem que ser (\d+)$/) do |arg1|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
-  
